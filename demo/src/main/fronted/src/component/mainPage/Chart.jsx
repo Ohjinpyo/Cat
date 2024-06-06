d8866c9b0f1f9d75b42017171b4660211be2c455
lee <20191748@edu.hanbat.ac.kr>
2024-06-06 21:56:47 +0900

    차트변경

--- demo/src/main/fronted/src/component/mainPage/Chart.jsx
+++ demo/src/main/fronted/src/component/mainPage/Chart.jsx
@@ -21,8 +21,43 @@ const ChartWrapper = styled.div`
     position: relative;
 `;
 
+const ButtonContainer = styled.div`
+    display: flex;
+    gap: 10px;
+    position: absolute;
+    bottom: 10px;
+    right: 10px;
+`;
+
+const Button = styled.button`
+    padding: 10px 15px;
+    font-size: 14px;
+    font-weight: bold;
+    color: white;
+    background-color: black;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: background-color 0.3s;
+
+    &:hover {
+        color: black;
+        background-color: #ffffff;
+    }
+
+    &:focus {
+        outline: none;
+        box-shadow: 0 0 0 3px rgb(12, 11, 11);
+    }
+
+    &:active {
+        background-color: #0c0b0b;
+    }
+`;
+
 const Chart = () => {
     const [chart, setChart] = useState(null);
+    const [sYear, setsYear] = useState(2017);
 
     useEffect(() => {
         const chart = createChart("container", {
@@ -42,13 +77,13 @@ const Chart = () => {
     }, []);
 
     useEffect(() => {
-        const fetchData = async () => {
+        const fetchData = async (year) => {
             try {
-                const response = await fetch(`http://3.38.101.95:8080/api/data/${year}`);
+                const response = await fetch(`http://localhost:8080/api/data/${year}`);
                 const data = await response.json();
 
                 const candlestickData = data.map(entry => ({
-                    time: new Date(entry.datetime).getTime() / 1000, // 시간을 UNIX 타임스탬프로 변환
+                    time: entry.datetime,
                     open: entry.open,
                     high: entry.high,
                     low: entry.low,
@@ -71,12 +106,19 @@ const Chart = () => {
             }
         };
 
-        fetchData();
-    }, [chart]);
+        fetchData(sYear);
+    }, [chart, sYear]);
 
     return (
         <ChartContainer>
             <ChartWrapper id="container" />
+            <ButtonContainer>
+                {[2017, 2018, 2019, 2020, 2021, 2022, 2023].map(year => (
+                    <Button key={year} onClick={() => {
+                        setsYear(year);
+                    }}>{year}</Button>
+                ))}
+            </ButtonContainer>
         </ChartContainer>
     );
 };
