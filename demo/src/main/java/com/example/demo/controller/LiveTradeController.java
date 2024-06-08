eb25cc72acad44bf548a3cc88e70e5187ec59d9e
Ohjinpyo <ojin21812181@gmail.com>
2024-06-08 15:22:52 +0900

    자동매매 수정

--- demo/src/main/java/com/example/demo/controller/LiveTradeController.java
+++ demo/src/main/java/com/example/demo/controller/LiveTradeController.java
@@ -51,7 +51,7 @@ public class LiveTradeController {
 
             // 파이썬 스크립트 실행
             String pythonScriptPath = "/home/ec2-user/ttttt/test_06_07.py";
-            ProcessBuilder processBuilder = new ProcessBuilder("python", pythonScriptPath, username, user.getApikey(), user.getApisecret());
+            ProcessBuilder processBuilder = new ProcessBuilder("python", pythonScriptPath, "ojp", user.getApikey(), user.getApisecret());
             Process process = processBuilder.start();
 
             // 실행 결과 출력
