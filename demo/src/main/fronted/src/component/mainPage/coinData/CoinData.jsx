c2b2d812decb9d2a5f50b5c951a2fd220446ea30
Ohjinpyo <ojin21812181@gmail.com>
2024-08-20 13:26:07 +0900

    코인 이미지

--- demo/src/main/fronted/src/component/mainPage/coinData/CoinData.jsx
+++ demo/src/main/fronted/src/component/mainPage/coinData/CoinData.jsx
@@ -1,14 +1,25 @@
+import BTC from "/demo/src/main/fronted/src/component/image/BTC.png"
+import ETH from "/demo/src/main/fronted/src/component/image/BTC.png"
+import ADA from "/demo/src/main/fronted/src/component/image/BTC.png"
+import BNB from "/demo/src/main/fronted/src/component/image/BTC.png"
+import XRP from "/demo/src/main/fronted/src/component/image/BTC.png"
+import DOGE from "/demo/src/main/fronted/src/component/image/BTC.png"
+import DOT from "/demo/src/main/fronted/src/component/image/BTC.png"
+import LTC from "/demo/src/main/fronted/src/component/image/BTC.png"
+import LINK from "/demo/src/main/fronted/src/component/image/BTC.png"
+import BCH from "/demo/src/main/fronted/src/component/image/BTC.png"
+
 const coinData = [
-    { name: "BTC", price: "$60,000", change: "+5%", volume: "100,000 BTC" },
-    { name: "ETH", price: "$2,500", change: "-2%", volume: "50,000 ETH" },
-    { name: "ADA", price: "$1.50", change: "+3%", volume: "80,000 ADA" },
-    { name: "BNB", price: "$500", change: "+4%", volume: "70,000 BNB" },
-    { name: "XRP", price: "$1.20", change: "+2%", volume: "120,000 XRP" },
-    { name: "DOGE", price: "$0.35", change: "+6%", volume: "200,000 DOGE" },
-    { name: "DOT", price: "$40", change: "+3%", volume: "60,000 DOT" },
-    { name: "LTC", price: "$200", change: "+1%", volume: "40,000 LTC" },
-    { name: "LINK", price: "$50", change: "+4%", volume: "90,000 LINK" },
-    { name: "BCH", price: "$800", change: "+2%", volume: "30,000 BCH" },
+    { logo: BTC, name: "BTC", price: "$60,000", change: "+5%", volume: "100,000 BTC" },
+    { logo: ETH, name: "ETH", price: "$2,500", change: "-2%", volume: "50,000 ETH" },
+    { logo: ADA, name: "ADA", price: "$1.50", change: "+3%", volume: "80,000 ADA" },
+    { logo: BNB, name: "BNB", price: "$500", change: "+4%", volume: "70,000 BNB" },
+    { logo: XRP, name: "XRP", price: "$1.20", change: "+2%", volume: "120,000 XRP" },
+    { logo: DOGE, name: "DOGE", price: "$0.35", change: "+6%", volume: "200,000 DOGE" },
+    { logo: DOT, name: "DOT", price: "$40", change: "+3%", volume: "60,000 DOT" },
+    { logo: LTC, name: "LTC", price: "$200", change: "+1%", volume: "40,000 LTC" },
+    { logo: LINK, name: "LINK", price: "$50", change: "+4%", volume: "90,000 LINK" },
+    { logo: BCH, name: "BCH", price: "$800", change: "+2%", volume: "30,000 BCH" },
     // 원하는 만큼 코인 데이터를 추가하세요
 ];
 
