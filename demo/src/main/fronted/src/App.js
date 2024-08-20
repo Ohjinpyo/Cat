3717568e3cc3c4a05e752d6b7cd6c77c61603b55
Ohjinpyo <ojin21812181@gmail.com>
2024-08-20 20:39:29 +0900

    페이지 변경

--- demo/src/main/fronted/src/App.js
+++ demo/src/main/fronted/src/App.js
@@ -21,7 +21,7 @@ const FixedHead = styled.div`
   width: 100%;
   z-index: 1000;
   background-color: white;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 2px 6px rgba(0, 0, 0, 0.1);
 `;
 
 const Content = styled.div`
