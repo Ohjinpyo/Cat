cb10329e4815c59ef42359b12c2d200d4e4793f3
ojin2 <ojin21812181@gmail.com>
2024-11-27 22:41:47 +0900

    자동매매 상태 ON/OFF 표시 변경

--- demo/src/main/fronted/src/component/page/autoTrading/AutoTradingPage.jsx
+++ demo/src/main/fronted/src/component/page/autoTrading/AutoTradingPage.jsx
@@ -14,7 +14,6 @@ const ImageContainer = styled.div`
     align-items: center;
     justify-content: center;
     margin-top: 20px;
-    border-top: 1px solid black;
     img{
         max-width: 100%;
         max-height: 100%;
@@ -27,7 +26,6 @@ const ExeContainer = styled.div`
     flex-direction: column;
     align-items: center;
     margin-top: 20px;
-    border-top: 1px solid black;
 `;
 
 const ButtonContainer = styled.div`
